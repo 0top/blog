0154f1693a485c2eaffaf3bce36a58d8a147eae7
zerotop <qk122.5.25@icloud.com>
2018-05-28 22:46:01 +0800

    admin

---
+++ src/main/java/top/zerotop/blog/dao/AdminMapper.java
@@ -0,0 +1,21 @@
+package top.zerotop.blog.dao;
+
+import org.apache.ibatis.annotations.Param;
+
+import top.zerotop.blog.entity.Admin;
+
+public interface AdminMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Admin record);
+
+    int insertSelective(Admin record);
+
+    Admin selectByPrimaryKey(Integer id);
+    
+    Admin selectByUsernameAndPassword(@Param("username")String username,@Param("password")String password);
+
+    int updateByPrimaryKeySelective(Admin record);
+
+    int updateByPrimaryKey(Admin record);
+}
--- src/main/java/top/zerotop/blog/releam/AdminRealm.java
+++ src/main/java/top/zerotop/blog/releam/AdminRealm.java
@@ -3,27 +3,58 @@ package top.zerotop.blog.releam;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import top.zerotop.blog.dao.AdminMapper;
+import top.zerotop.blog.entity.Admin;
 
 /**
  *@author 作者: zerotop
  *@createDate 创建时间: 2018年5月23日下午11:14:50
  */
 public class AdminRealm extends AuthorizingRealm {
+	
+	@Autowired
+	private AdminMapper adminDao;
+	
+	public String getName(){
+		return "AdminRealm";
+	}
 
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-		System.out.println("身份认证");
+		System.out.println("授权");
 		return null;
 	}
 
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		System.out.println("授权");
+		System.out.println("认证");
+		
+		String username = token.getPrincipal().toString();
+		String password = token.getCredentials().toString();
+		
+		Admin  admin = new Admin();
+		admin.setUsername(username);
+		admin.setPassword(password);
+		
+		admin = adminDao.selectByUsernameAndPassword(username, password);
 
-		return null;
+
+		if(null == admin){
+			throw new UnknownAccountException();
+		}
+		else{
+			
+			AuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo( username, password, getName());
+            return authenticationInfo;
+            
+		}
 	}
 
 }
