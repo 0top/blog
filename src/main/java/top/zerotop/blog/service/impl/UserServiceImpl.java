20ee74fb9eda541bfe408f1cf4880bc0e1a8f8eb
zerotop <qk122.5.25@icloud.com>
2018-08-02 23:35:57 +0800

    new dependency

--- src/main/java/top/zerotop/blog/service/impl/UserServiceImpl.java
+++ src/main/java/top/zerotop/blog/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package top.zerotop.blog.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import top.zerotop.blog.dao.AdminMapper;
 import top.zerotop.blog.entity.Admin;
@@ -10,6 +11,7 @@ import top.zerotop.blog.serivce.UserService;
  *@author 作者: zerotop
  *@createDate 创建时间: 2018年6月5日下午11:01:33
  */
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
