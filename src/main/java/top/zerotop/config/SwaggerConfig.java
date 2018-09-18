425fa68def96042b679844444ba71d07ac0da5a2
0top <qk122.5.25@icloud.com>
2018-09-18 23:12:47 +0800

    update swagger ui

--- src/main/java/top/zerotop/config/SwaggerConfig.java
+++ src/main/java/top/zerotop/config/SwaggerConfig.java
@@ -1,8 +1,6 @@
 package top.zerotop.config;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -15,9 +13,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
 @EnableSwagger2
-//@EnableWebMvc
+@EnableWebMvc
 public class SwaggerConfig  {
 
     @Bean
@@ -25,8 +22,7 @@ public class SwaggerConfig  {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(productApiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-//                .apis(RequestHandlerSelectors.basePackage("top.zerotop.blog.controller"))
+                .apis(RequestHandlerSelectors.basePackage("top.zerotop.blog.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
--- src/main/java/top/zerotop/config/SwaggerInterceptor.java
+++ src/main/java/top/zerotop/config/SwaggerInterceptor.java
@@ -4,18 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@Configuration
-@EnableWebMvc
+//
+//@Configuration
+//@EnableWebMvc
 class SwaggerInterceptor extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//        registry.addResourceHandler("/static/**")
+//                .addResourceLocations("classpath:/static/");
+//
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
