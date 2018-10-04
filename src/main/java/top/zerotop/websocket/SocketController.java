ef2c9212117953a5139c13fe45d052170852d62d
0top <qk122.5.25@icloud.com>
2018-10-04 22:39:51 +0800

    update

--- src/main/java/top/zerotop/websocket/SocketController.java
+++ src/main/java/top/zerotop/websocket/SocketController.java
@@ -1,6 +1,8 @@
 package top.zerotop.websocket;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,12 +12,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class SocketController {
 
     @Autowired
     private SimpMessagingTemplate template;
     private Message<String> res;
+
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     @MessageMapping("/getmsg")
     @SendTo("/topic/greeting")
@@ -25,10 +33,12 @@ public class SocketController {
         ResMsg msg = new ResMsg();
         msg.setMsg(payload.toString());
 
+        redisTemplate.opsForList().leftPush("/topic/msg", JSON.toJSONString(msg));
+        List<String> msgList = redisTemplate.opsForList().range("/topic/msg", 0, -1);
+
         template.convertAndSend("/topic/msg", msg);
 
-//        template.convertAndSendToUser("1","/topic/msg", msg );
-        System.out.println();
+        System.out.println("====new topic msg====");
 
         return msg;
     }
