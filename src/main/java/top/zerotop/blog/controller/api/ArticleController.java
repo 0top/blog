ecd041722d21334ea87f6e9fe6199a400d935305
0top <qk122.5.25@icloud.com>
2018-10-11 23:05:16 +0800

    update

--- src/main/java/top/zerotop/blog/controller/api/ArticleController.java
+++ src/main/java/top/zerotop/blog/controller/api/ArticleController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.shiro.authz.annotation.RequiresRoles;
@@ -16,11 +15,10 @@ import org.springframework.web.bind.annotation.*;
 import com.alibaba.fastjson.JSON;
 
 import top.zerotop.blog.controller.BaseController;
-import top.zerotop.blog.domain.Article;
-import top.zerotop.blog.serivce.BlogService;
+import top.zerotop.blog.db.model.Article;
+import top.zerotop.blog.service.BlogService;
 import top.zerotop.blog.util.PageConstrant;
-import top.zerotop.blog.util.ReMap;
-import top.zerotop.blog.util.ReqJson;
+import top.zerotop.blog.util.Result;
 import top.zerotop.exception.BlogException;
 
 /**
@@ -29,8 +27,7 @@ import top.zerotop.exception.BlogException;
  */
 @Api(value = "文章列表")
 @RestController
-//@RestController(value = "/article", produces =  {MediaType.APPLICATION_JSON_VALUE})
-@RequestMapping(value = "/article", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping(value = "/article", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
 public class ArticleController extends BaseController {
 
     @Autowired
@@ -39,8 +36,8 @@ public class ArticleController extends BaseController {
     @ApiOperation(value = "获取单篇文章的内容",
             notes = "根据id获取文章内容")
     @GetMapping(value = "/get/{id}")
-    @ResponseBody
-    public Article getArticleById(@PathVariable("id") int id) {
+    public Article getArticleById(@ApiParam(value = "文章id")
+                                  @PathVariable("id") int id) {
 
         return blogService.getArticleById(id);
     }
@@ -49,51 +46,32 @@ public class ArticleController extends BaseController {
             notes = "添加文章")
 //	@RequiresRoles("admin")
     @PostMapping(value = "/insert")
-    @ResponseBody
-    public String insertArticle(HttpServletRequest req,
+    public Result insertArticle(HttpServletRequest req,
                                 @ApiParam(value = "文章")
                                 @RequestBody Article article) throws BlogException {
 
         int id = blogService.insertArticle(article);
         article.setId(id);
 
-        return JSON.toJSONString(article);
+        return new Result("获取文章",article);
     }
 
     @ApiOperation(value = "更新文章",
             notes = "更新文章")
-    @RequiresRoles("admin")
+//    @RequiresRoles("admin")
     @PostMapping(value = "/update")
-    public String updateArticle(
-            @ApiParam(value = "文章内容")
-            @RequestBody Article article) {
-
-
-//		Article article = JSON.parseObject(json, Article.class);
-
-		blogService.updateArticleSelective(article);
-
-        System.out.println(article.toString());
-
-        return "success";
+    public Result updateArticle(@ApiParam(value = "文章内容")
+                                @RequestBody Article article) {
+        blogService.updateArticleSelective(article);
+        return new Result("更新成功");
     }
 
-    /**
-     * 获取文章列表
-     *
-     * @param req
-     * @return
-     */
     @ApiOperation(value = "获取文章列表",
-            notes = "分页查询文章列表",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            notes = "分页查询文章列表")
     @GetMapping(value = "/list")
-    public String listArticle(HttpServletRequest req,
+    public Result listArticle(HttpServletRequest req,
                               @ApiParam(value = "pagenum")
                               @RequestParam("pagenum") int pagenum) {
-
-        List<Article> articleList = blogService.listArticle(pagenum, PageConstrant.pagesize);
-
-        return JSON.toJSONString(articleList);
+        return new Result("获取文章列表",blogService.listArticle(pagenum, PageConstrant.pagesize));
     }
 }
